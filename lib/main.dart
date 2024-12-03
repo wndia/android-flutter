1312f34ab08150046152b4b07fa695fff1278aca
wndia <wndiaalwi@gmail.com>
2024-12-03 12:38:47 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
+import 'splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:perpus/views/library_view.dart';
-import 'package:perpus/controllers/book_controller.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(CinepolisApp());
 }
 
-class MyApp extends StatelessWidget {
+class CinepolisApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => BookController(),
-      child: MaterialApp(
-        title: 'Library App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LibraryView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Cinepolis App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SplashScreen(),
     );
   }
 }
